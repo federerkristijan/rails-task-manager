6daac89dc374d1b19f62a2349c8b6bb7fb81e7d1
Kristijan Federer <federer.kristijan@gmail.com>
2022-02-15 16:40:41 +0100

    delete missing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # index route
+  get "tasks", to: "tasks#index"
+
+  # create route
+  post "tasks", to: "tasks#create"
+  get "tasks/new", to: "tasks#new", as: :new_task
+
+  # edit route
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+
+  # show route
+  get "tasks/:id", to: "tasks#show", as: :task
 end
